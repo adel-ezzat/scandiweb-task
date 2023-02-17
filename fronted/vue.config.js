cbcb82cf5a9c4d9f2351b24f2cdd714a82bfcab9
adelezzatl <31221738+adelezzatl@users.noreply.github.com>
2023-02-17 19:12:11 +0200

    fixes

--- fronted/vue.config.js
+++ fronted/vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  // publicPath: 'fronted/dist',
+  publicPath: 'fronted/dist',
   chainWebpack: config => {
     if(config.plugins.has('extract-css')) {
       const extractCSSPlugin = config.plugin('extract-css')
